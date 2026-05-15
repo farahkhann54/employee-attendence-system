c7adcb6c08face925b24d6491d866e4eba90fdfe
Farah Khan <farahkhann7342@gmail.com>
2026-05-15 17:21:11 +0500

    deleted extra comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 extend: {
   colors: {
-    // Semicolon (;) ki jagah hamesha comma (,) lagayein
+        
     'glass-bg': 'rgba(17, 24, 39, 0.6)', 
     'glass-border': 'rgba(255, 255, 255, 0.1)',
   }
