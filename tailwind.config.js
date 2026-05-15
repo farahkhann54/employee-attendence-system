4ea70197973ec36af2306a3ca29b192bd36570a4
Farah Khan <farahkhann7342@gmail.com>
2026-05-15 17:24:07 +0500

    tailwind file fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
-extend: {
-  colors: {
-        
-    'glass-bg': 'rgba(17, 24, 39, 0.6)', 
-    'glass-border': 'rgba(255, 255, 255, 0.1)',
-  }
-}
+
 module.exports = {
   theme: {
     extend: {
       colors: {
         'glass-bg': 'rgba(17, 24, 39, 0.6)',
         'glass-border': 'rgba(255, 255, 255, 0.1)',
-      }
-    }
-  }
+      },
+    },
+  },
+  plugins: [],
 };
