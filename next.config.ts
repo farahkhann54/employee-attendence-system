7084d4b9fa6c7bc3d59c7801438937bcc710712b
Farah Khan <farahkhann7342@gmail.com>
2026-05-17 12:30:56 +0500

    removed esLint

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // Yeh line build ko fail hone se rokegi
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // ESLint errors ko bhi ignore karega build ke waqt
-    ignoreDuringBuilds: true,
-  },
-  // Turbopack settings (Optional)
-  transpilePackages: ['lucide-react'],
 }
 
-module.exports = nextConfig
+export default nextConfig
